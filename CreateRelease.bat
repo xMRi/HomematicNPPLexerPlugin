f0b0edd2c206846b1b14aabc07eb3841019dda8e
Martin Richter <xmrichter@gmail.com>
2026-01-01 21:05:55 +0100

    Changed version to 1.2.0.30 - Changed version to 1.2.030 - Fix batch file to pack release files

--- CreateRelease.bat
+++ CreateRelease.bat
@@ -1,4 +1,4 @@
 @for %%Name IN (Win32 x64 ARM64) ( 
-  7z a -bd -bso0 Bin\HomeMaticNPPLexer_%Name%.zip Bin\%Name%\Release\*.dll Bin\%Name%\Release\*.xml 
+  7z a -bd -bso0 Bin\HomeMaticNPPLexer_%Name%.zip .\Bin\%Name%\Release\*.dll .\Bin\%Name%\Release\*.xml 
 )
-
+@Echo Done
